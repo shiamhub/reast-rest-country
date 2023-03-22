b6bfc1e6591d555eaa49dd597f3cfd329197fc6b
shiamhub <skshiam@gmail.com>
2023-03-22 23:12:26 +0600

    Header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,22 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <h2>start</h2>
-            <nav className='h-nav'>
-                <a href="/home">Home</a>
-                <a href="/home">Blogs</a>
-                <a href="/home">About</a>
-                <a href="/home">Us</a>
-            </nav>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
         </div>
     );
 };
